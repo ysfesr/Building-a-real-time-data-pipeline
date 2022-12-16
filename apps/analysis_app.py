90dc21459374c7d239566c5a61860870b20d8b75
Abdelali Bendra <110561842+bendraabdelali@users.noreply.github.com>
2022-12-16 08:22:53 +0100

    dev 0

--- apps/analysis_app.py
+++ apps/analysis_app.py
@@ -4,17 +4,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
-
 spark:SparkSession = SparkFactory.Factory(
     YamlLoader("config.yaml").get_data(), 
     IniLoader("app.ini").get_data()
 ).get()
 
-
 df = spark.readStream\
    .format("delta")\
    .load("path", "s3a://datalake/Ecommerce/all_sessions")
-
 
 # Indique le nombre total de visiteurs uniques
 query1 = df.select(count("*").alias("product_views"), count(col("fullVisitorId").distinct()).alias("unique_visitors"))\
@@ -37,7 +34,6 @@ query3 = df.filter(col("type") == 'PAGE') \
     .withColumn("rank", rank().over(Window.partitionBy().orderBy(col("product_views").desc()))) \
     .filter(col("rank") <= 5)\
     .writeStream.format("console").start().awaitTermination()
-
 
 # Elimine les vues de produit en double pour les visiteurs qui ont consulté un produit plusieurs fois
 query4 = df.filter(col("type") == "PAGE")\

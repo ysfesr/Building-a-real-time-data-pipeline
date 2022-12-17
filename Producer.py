531be09dcd8ccbb56e7c73e7f33b378afbe73e85
beneji0 <beneji.projects@gmail.com>
2022-12-17 15:37:56 +0100

    Refactoring the stream generator (kafka producer)

--- Producer.py
+++ Producer.py
@@ -5,7 +5,7 @@ import json
 import time
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9093'],
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'), api_version=(2,0,2))
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 def kafka_producer(data):
     try:
